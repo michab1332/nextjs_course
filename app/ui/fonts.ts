33607735506d502bc44e8f879462c77155cce7e6
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2024-03-27 17:56:47 +0100

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Poppins, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({ subsets: ['latin'], weight: ['200', '400'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
